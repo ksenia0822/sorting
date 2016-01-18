e85af1221d7912bd5c1080ed71a3d142d5dbd47a
intersim <emily.intersimone@gmail.com>
2016-01-18 14:21:19 -0500

    updated mergesort.spec

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -9,3 +9,22 @@ describe('Split Array', function(){
         expect( split([1,3,5,2,4]) ).toEqual( [[1,3],[5,2,4]] );
     });
 });
+
+describe('MergeSort', function(){
+    it('is able to sort an array', function(){
+        expect( mergeSort([1,3,5,2,4]) ).toEqual( [1,2,3,4,5] );
+    });
+
+    it('calls the merge function', function(){
+    spyOn(window, 'merge').and.callThrough();
+    expect(mergeSort([1,3,5,2,4])).toEqual([1,2,3,4,5]);
+    expect(merge).toHaveBeenCalled();
+  });
+
+    it('calls the split function', function(){
+    spyOn(window, 'merge').and.callThrough();
+    expect(mergeSort([1,3,5,2,4])).toEqual([1,2,3,4,5]);
+    expect(split).toHaveBeenCalled();
+  });
+    
+});
